c5dc90303127d037349b10894785e8d6abf3283f
Scott Olsen <scott.c.olsen@oracle.com>
2018-04-04 16:07:45 -0400

    Add Option generating and processing functions and types.

--- src/Epigraph.hs
+++ src/Epigraph.hs
@@ -1,50 +1,122 @@
 module Epigraph where
 
-import qualified Epigraph.Types as Option
-
 import qualified Data.List as L
 import System.Environment
 import Data.Maybe 
 
--- Functions
+type OptionLong      = String
+type OptionShort     = Maybe Char
+type OptionPredicate = Maybe (String -> Bool)
+type OptionHelp      = Maybe String
+type OptionCoercion  = Maybe (String -> String)
+type OptionStub      = (String, OptionMode)
+type OptionSpec      = (OptionLong, OptionMode, OptionShort, OptionPredicate, OptionCoercion, OptionHelp)
 
---Determine if a string is an option.
-isOption :: String -> Option -> Bool
-isOption x (Option s y _) = fromMaybe False $ (||) 
-                            <$> Just (x == s) 
-                            <*> ((==head x) <$> y)
+data OptionMode = Argument
+                | Flag
+                deriving (Eq, Show)
 
-getOption :: String -> [Option] -> Maybe Option
-getOption s os = L.find (isOption s) os
+data Argument = StringArgument String
+              | BoolArgument Bool
+              | NoArgument
+              deriving (Eq, Show)
 
---getOptions :: [String] -> [Option] -> [(String, b)]
---getOptions (x:y:xs) os 
---           | any (isOption x) os = 
---               case optionType <$> opt of
---                 Just Argument -> (:) (Runtime Arg y) $ getOptions xs os
---                 Just Flag     -> (:) (Runtime Flg) $ getOptions (y:xs) os
---           | otherwise  = getOptions (y:xs) os
---           where opt = getOption x os
---getOptions (x:[]) os = (Runtime (getOption x os) Nothing):getOptions [] os
---getOptions []     _ = []
+data Option = Option { long         :: String
+                     , mode         :: OptionMode
+                     , short        :: Maybe Char
+                     , predicate    :: (String -> Bool)
+                     , coercion     :: (String -> String)
+                     , help         :: Maybe String
+                     , hasPredicate :: Bool
+                     , hasCorecion  :: Bool
+                     } 
 
-mktuple :: Option -> String -> (String, b)
-mktuple o y 
-        | argument && validArgument y = (optionLong o, coerce o y) 
-        | otherwise                      = (optionLong o, True)
-        where argument = Option.mode o == Argument
+instance Show Option where
+  show (Option l m s _ _ h p c) = "Option: " 
+                                  ++ " long: " ++ show l 
+                                  ++ " type: " ++ show m 
+                                  ++ " short: " ++ show s
+                                  ++ " help: " ++ show h 
+                                  ++ " Has predicate: " ++  show p 
+                                  ++ " Has coercion: " ++  show c 
+                                  ++ "\n" 
 
-validArgument :: Option -> String -> Bool
-validArgument o y
-              | Option.predicate o == Nothing = True
-              | otherwise = fromMaybe False $ Option.predicate <*> pure y
+data ProcessedOption = Processed Option Argument
+                     | UnrecognizedOption String
+                     deriving Show
 
--- Coerce the argumet.
-coerce :: Option -> String -> Maybe String
-coerce o y
-       | Option.coercion == Nothing = y
-       | otherwise = Option.coercion <*> pure y
+-- | Construct Options
 
-quickOptions :: [(String, OptionType)] -> [Option]
-quickOptions (x:xs) = (Option (fst x) (snd x) Nothing Nothing Nothing Nothing):quickOptions xs
+quickOptions :: [OptionStub] -> [Option]
+quickOptions (x:xs) = (Option (fst x) (snd x) s (const True) id Nothing False False):quickOptions xs
+                    where s = setShort (fst x) (x:xs)
 quickOptions [] = []
+
+options :: [OptionSpec] -> [Option]
+options ((x, y, z, m, n, k):xs) = (Option x y z (fromMaybe (const True) m) (fromMaybe id n) k p c):options xs
+                                where p = isJust m
+                                      c = isJust n
+options [] = []
+
+-- | Construct Option Argument Pairs
+
+setArgument :: Option -> String -> Argument
+setArgument o s 
+            | flag                      = BoolArgument True
+            | s == ""                   = NoArgument
+            | argument && predicate o s = StringArgument $ coercion o s
+            | otherwise                 = NoArgument
+            where argument = mode o == Argument
+                  flag     = mode o == Flag
+
+-- | Get CLI Values
+
+isOption :: String -> Option -> Bool
+isOption x o = x == long o || fromMaybe False ((==head x) <$> short o)
+
+getOption :: String -> [Option] -> String -> ProcessedOption
+getOption s os a
+          | null go   = UnrecognizedOption s
+          | otherwise = Processed (head go) $ setArgument (head go) a
+          where go = filter (isOption s) os
+
+isUnrecognized :: ProcessedOption -> Bool
+isUnrecognized (UnrecognizedOption _) = True
+isUnrecognized _ = False
+
+hasMode :: ProcessedOption -> Maybe OptionMode
+hasMode (Processed o a) = Just $ mode o
+hasMode _               = Nothing
+
+getOptions :: [String] -> [Option] -> [ProcessedOption]
+getOptions (s:y:ss) os
+           | hasMode go == Just Argument = go:getOptions ss os
+           | otherwise              = go:getOptions (y:ss) os 
+           where go = getOption s os y
+getOptions (s:[]) os = (getOption s os ""):[]
+getOptions [] _      = []
+
+recognized :: [ProcessedOption] -> [ProcessedOption]
+recognized xs = filter (not . isUnrecognized) xs
+recognized [] = []
+
+-- | Autopopulate Option short name
+
+firstLetters :: [String] -> [Char]
+firstLetters (x:xs) = (head x):firstLetters xs
+firstLetters [] = []
+
+isUnique :: Char -> [Char] -> Bool
+isUnique x cs = (length $ filter (\c -> c == x) cs) == 1
+
+isUniqueShort :: String -> [String] -> Bool
+isUniqueShort x xs = isUnique (head x) (firstLetters xs)
+
+optionNames :: [(String, OptionMode)] -> [String]
+optionNames (x:xs) = (fst x):optionNames xs  
+optionNames []     = []
+
+setShort :: String -> [(String, OptionMode)] -> Maybe Char
+setShort x os 
+         | (isUniqueShort x $ optionNames os) == True = Just $ head x
+         | otherwise = Nothing
